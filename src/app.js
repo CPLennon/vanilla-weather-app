159e2a007c32588b5574a20dc07a01174fc4e202
CPLennon <caroline8pic@gmail.com>
2021-09-19 12:14:57 +0930

    added weather data from API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
-function displayTemperature(response) {
-  let mainTemp = document.querySelector("#main-temp");
-  mainTemp.innerHTML = Math.round(response.data.main.temp);
+function displayWeather(response) {
+  document.querySelector("#main-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#weather-desc").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
 }
 
 let apiKey = "b5a777ab71fc602967504eb64daf1657";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=auckland&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+axios.get(apiUrl).then(displayWeather);
