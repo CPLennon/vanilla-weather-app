f493349eb0d475478d7c82e133417c1582a91023
CPLennon <caroline8pic@gmail.com>
2021-09-26 12:33:07 +0930

    Changed API icons to local images

--- src/app.js
+++ src/app.js
@@ -1,4 +1,22 @@
+function formatDate(timestamp) {
+  let now = new Date(timestamp);
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#main-temp").innerHTML = Math.round(
     response.data.main.temp
   );
@@ -8,9 +26,19 @@ function displayWeather(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
+  document
+    .querySelector("#main-image")
+    .setAttribute("src", `images/${response.data.weather[0].icon}.svg`);
+  document
+    .querySelector("#main-image")
+    .setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "b5a777ab71fc602967504eb64daf1657";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=auckland&appid=${apiKey}&units=metric`;
+let city = "Adelaide";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayWeather);
